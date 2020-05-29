ce5d4790aeef9f2e617ff2e2751ec4c1b7abf75f
xsrf <git@xsrf.de>
2020-05-29 21:06:21 +0200

    move instructions to top comment

--- src/hub75debug.cpp
+++ src/hub75debug.cpp
@@ -1,12 +1,40 @@
-#include <Arduino.h>
-#include <SPI.h>
-#define PIN_NC 255
+/*
+
+HUB75 Debug Instructions
+========================
+
+This short program will run two basic tests on a typical HUB75 LED matrix panel.
+
+Test 1
+------
+This test will help you identify how the panel addresses its rows and also how many
+rows need to be addressed.
+When the first test runs, you should see white lines on your panel. 
+How many rows are they apart?
+If every N-th row is lit, you have a 1/N panel. e.g. if every 4th row is lit, it's a 1/4 panel.
+Also, which line is lit? Note that the Top-Side is usually marked on the backside of the panel.
+* If the FIRST row is lit, you have a STRAIGHT Mux.
+* If the SECOND row is lit, you have a BINARY Mux.
+* If the THIRD row is lit, you have a SHIFTREG_ABC Mux with LATCH on PIN_B.
+* If the FOURTH row is lit, you have a SHIFTREG_ABC Mux without LATCH and Shift on falling edge.
+* If the FIFTH row is lit, you have a SHIFTREG_ABC Mux without LATCH and Shift on rising edge.
+
+Test 2
+------
+This test will help you identify how the incoming data is mapped to the LEDs of each scan-line.
+Pixel by pixel will be enabled to show the mapping pattern evolving.
+The test will run twice - first fast than slow.
+You can skip this test by pressing any key (via Serial connection).
+
+*/
 
 /* 
   ====================================================
   === MODIFY THIS BELOW TO FIT YOUR PANEL / WIRING === 
   ====================================================
 */
+// Use PIN_NC for unconnected Pins
+#define PIN_NC 255
 
 // Matrix Dimensions
 #define MATRIX_WIDTH  64
@@ -29,6 +57,8 @@
   ====================================================
 */
 
+#include <Arduino.h>
+#include <SPI.h>
 
 enum mux_patterns {BINARY, STRAIGHT, SHIFTREG_ABC};
 uint32_t bpad = (MATRIX_WIDTH*MATRIX_HEIGHT*3)/2;
@@ -69,17 +99,7 @@ void setup() {
 
 void loop() {
   // Step 1: Get MUX-Pattern... 1/2, 1/4, 1/8, ...
-  Serial.println("");
   Serial.println("Test 1: Mux/Scan");
-  Serial.println("----------------");
-  Serial.println("You should see white lines on your panel. How many rows are they apart?");
-  Serial.println("If every N-th row is lit, you have a 1/N panel. e.g. if every 4th row is lit, it's a 1/4 panel.");
-  Serial.println("Also, which line is lit? Note that the Top-Side is usually marked on the backside of the panel.");
-  Serial.println("* If the FIRST row is lit, you have a STRAIGHT Mux.");
-  Serial.println("* If the SECOND row is lit, you have a BINARY Mux.");
-  Serial.println("* If the THIRD row is lit, you have a SHIFTREG_ABC Mux with LATCH on PIN_B.");
-  Serial.println("* If the FOURTH row is lit, you have a SHIFTREG_ABC Mux without LATCH and Shift on falling edge.");
-  Serial.println("* If the FIFTH row is lit, you have a SHIFTREG_ABC Mux without LATCH and Shift on rising edge.");
   lastmillis = millis();
   while(millis() - lastmillis < 5e3) {
     fillBuffer(bpad,0);
@@ -89,11 +109,7 @@ void loop() {
   }
 
   // Step 2: Scan-Pattern
-  Serial.println("");
   Serial.println("Test 2: Pattern");
-  Serial.println("---------------");
-  Serial.println("Pixel by pixel will be enabled now to show you how the data is mapped to the scan-lines.");
-  Serial.println("You can slow down / skip this test by pressing any key.");
 
   delay(1e3);
   uint16_t cnt = 0;
